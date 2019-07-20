8b6e490178674e372a9868d7daabc397ac012fad
trenchbl <bl.trenchev@gmail.com>
2019-07-20 14:15:48 +0300

    Initial bare minimum version. It requires installed database.

---
+++ src/main/java/com/trenchbl/blogger/bloggerjava/BloggerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.trenchbl.blogger.bloggerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerJavaApplication.class, args);
+	}
+
+}
